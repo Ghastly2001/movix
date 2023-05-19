c69c70c01040d20189eb896f2f1f47d35bc6ac2c
Ghastly2001 <divyanshverma18may@gmail.com>
2023-05-19 17:05:00 +0530

    New commit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -27,7 +27,7 @@ const Footer = () => {
                     aliqua. Ut enim ad minim veniam, quis nostrud exercitation
                     ullamco laboris nisi ut aliquip ex ea commodo consequat.
                     Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur.
+                    esse cillum dolore eu fugiat nulla pariatur
                 </div>
                 <div className="socialIcons">
                     <span className="icon">
